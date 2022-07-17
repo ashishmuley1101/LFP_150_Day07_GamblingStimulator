9f8ec3659a57a4ed24deee52f1b62684257d5cbb
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-07-17 13:45:11 +0530

    Day 07 Gambling Stimulator

---
+++ src/com/bridgelab/gamblingstimulator/GamblingStimulatorUC1.java
@@ -0,0 +1,15 @@
+package com.bridgelab.gamblingstimulator;
+
+public class GamblingStimulatorUC1 {
+	static int dailyStake = 100;
+    static int betInDollar = 1;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Gambling Simulator.....!!");
+		
+		System.out.println("Total daily Stake is : " + dailyStake);
+        System.out.println("Betting Dollar for every game : " + betInDollar);
+
+	}
+
+}
