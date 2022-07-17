3eaffd501c5f74e4c877afe610dc512f1c31a41a
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-07-17 14:29:22 +0530

    gamblingstimulatorUC4

--- src/com/bridgelab/gamblingstimulator/GamblingStimulator.java
+++ src/com/bridgelab/gamblingstimulator/GamblingStimulator.java
@@ -5,22 +5,33 @@ public class GamblingStimulator {
 	public static int dailyStake = 100;
 	public static int betInDollar = 1;
 	public static final int win = 1;
+	public static int totalAmountWonFor20days = 0;
+	public static int totalAmountLostFor20days = 0;
 
 	public static void main(String[] args) {
 		System.out.println("************Welcome To Gambling Simulator***************");
-		
-		while (dailyStake > 50 && dailyStake != 150 ) {
-			int result = (int) ((Math.random() * 10) % 2);
-			if(result == win) {
-				dailyStake++;
-				System.out.println("Won");
-			}else {
-				dailyStake--;
-				System.out.println("Loose");
+
+		for (int day = 1; day <= 20; day++) {
+			while (dailyStake >= 49 && dailyStake <= 149) {
+				int result = (int) ((Math.random() * 10 % 2) + 1);
+				if (result == win) {
+					dailyStake++;
+					System.out.println("Won");
+				} else {
+					dailyStake--;
+					System.out.println("Loose");
+				}
+
+				if (dailyStake == 150) {
+					totalAmountWonFor20days += 50;
+				} else if (dailyStake == 50) {
+					totalAmountLostFor20days -= 50;
+				}
+
 			}
 		}
 
-		System.out.println("Total daily Stake is : " + dailyStake);
-        System.out.println("Betting Dollar for every game : " + betInDollar);
+		System.out.println("Total amount won for 20 Days : " + totalAmountWonFor20days + " $");
+		System.out.println("Total amount lost for 20 Days : " + totalAmountLostFor20days + " $");
 	}
 }
