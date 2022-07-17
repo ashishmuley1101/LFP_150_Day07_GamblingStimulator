8ff25172635b9b02e4d47935b0f726b25bed466c
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-07-17 14:07:12 +0530

    gamblingstimulatorUC3

--- src/com/bridgelab/gamblingstimulator/GamblingStimulator.java
+++ src/com/bridgelab/gamblingstimulator/GamblingStimulator.java
@@ -6,19 +6,20 @@ public class GamblingStimulator {
 	public static int betInDollar = 1;
 	public static final int win = 1;
 
-
 	public static void main(String[] args) {
 		System.out.println("************Welcome To Gambling Simulator***************");
-
-		int result = (int) ((Math.random() * 10) % 2);
-		if(result == win) {
-			dailyStake++;
-			System.out.println("Won");
-		}else {
-			dailyStake--;
-			System.out.println("Loose");
-		}
 		
+		while (dailyStake > 50 && dailyStake != 150 ) {
+			int result = (int) ((Math.random() * 10) % 2);
+			if(result == win) {
+				dailyStake++;
+				System.out.println("Won");
+			}else {
+				dailyStake--;
+				System.out.println("Loose");
+			}
+		}
+
 		System.out.println("Total daily Stake is : " + dailyStake);
         System.out.println("Betting Dollar for every game : " + betInDollar);
 	}
